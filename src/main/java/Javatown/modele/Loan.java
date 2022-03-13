84c98d39c5d591ee84301a6a4aa5d1f265e23ec4
Samir_Badi <samirbadi1@outlook.com>
2022-03-13 18:52:38 -0400

    Add the class Loan to the couche modele

---
+++ src/main/java/Javatown/modele/Loan.java
@@ -0,0 +1,56 @@
+package Javatown.modele;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    @ToString.Exclude
+    private Client client;
+
+    @Basic
+    private java.sql.Date dateOfLoan;
+    @Basic
+    private java.sql.Date dateOfReturn;
+
+    //private Document document;
+    public Loan(Client client, String dateOfLoan, AbstractDocument document) {
+        this.client = client;
+        this.dateOfLoan = java.sql.Date.valueOf(dateOfLoan);
+        this.dateOfReturn = java.sql.Date.valueOf(returnDate(dateOfLoan, document.loanTimeInDay()));
+    }
+
+    private String returnDate(String date, int days) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date myDate = null;
+        try {
+            myDate = format.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        myDate = addDays(myDate, days);
+        return format.format(myDate);
+    }
+
+    private Date addDays(Date myDate, int days) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(myDate);
+        cal.add(Calendar.DATE, days);
+        return cal.getTime();
+    }
+}
