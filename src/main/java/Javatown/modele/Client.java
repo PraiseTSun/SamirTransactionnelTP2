fb9839d8269be975e5a5210acbb13e1071096d50
Samir_Badi <samirbadi1@outlook.com>
2022-03-13 18:52:06 -0400

    Add the class Client that expends on AbstractUser to the couche modele

---
+++ src/main/java/Javatown/modele/Client.java
@@ -0,0 +1,31 @@
+package Javatown.modele;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Client extends AbstractUser{
+    private String resident;
+    @OneToMany(mappedBy = "client", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Loan> loans;
+
+
+    public Client(String firstName, String lastName, String password, String resident) {
+        super(firstName, lastName, password);
+        this.resident = resident;
+        this.loans = new ArrayList<>();
+    }
+
+    public boolean isReseidentOfJavatown(){
+        return resident.equals("Javatows");
+    }
+}
