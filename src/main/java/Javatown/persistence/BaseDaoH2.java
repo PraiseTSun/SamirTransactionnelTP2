0fdeba6e8bcbe3d4dfbcdb10e3d39752cb59a453
Samir_Badi <samirbadi1@outlook.com>
2022-03-13 19:00:52 -0400

    Add the generic method save to the abstract class BaseDaoH2

--- src/main/java/Javatown/persistence/BaseDaoH2.java
+++ src/main/java/Javatown/persistence/BaseDaoH2.java
@@ -1,5 +1,20 @@
 package Javatown.persistence;
 
-public abstract class BaseDaoH2 {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public abstract class BaseDaoH2 {
+    protected EntityManagerFactory emf = Persistence.createEntityManagerFactory("javatown.exe");
+
+    protected <T> void save(T t) {
+        final EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(t);
+
+        em.getTransaction().commit();
+        em.close();
+
+    }
 }
