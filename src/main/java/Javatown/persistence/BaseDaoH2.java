51ea554616cb9e019db407d351d496f16b1f19fe
Samir_Badi <samirbadi1@outlook.com>
2022-03-13 19:01:37 -0400

    Add the generic method findByPassword to the abstract class BaseDaoH2

--- src/main/java/Javatown/persistence/BaseDaoH2.java
+++ src/main/java/Javatown/persistence/BaseDaoH2.java
@@ -3,6 +3,7 @@ package Javatown.persistence;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 public abstract class BaseDaoH2 {
     protected EntityManagerFactory emf = Persistence.createEntityManagerFactory("javatown.exe");
@@ -23,6 +24,24 @@ public abstract class BaseDaoH2 {
         em.getTransaction().begin();
 
         var u = (T) em.find(t, id);
+
+        em.getTransaction().commit();
+        em.close();
+        return u;
+    }
+
+    protected <T> T findByPassword (Class<T> t, String firstName, String lastName, String password){
+        final EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        final TypedQuery<T> query = em.createQuery(
+                "select p from " + t.getName() + " p " +
+                        "where p.firstName = :userFirstName AND p.lastName = :userLastName AND p.password = :userPassword"
+                , t);
+        query.setParameter("userFirstName", firstName);
+        query.setParameter("userLastName", lastName);
+        query.setParameter("userPassword", password);
+        var u = (T) query.getSingleResult();
 
         em.getTransaction().commit();
         em.close();
