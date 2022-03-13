4e62dc6efd58b51edaf115cd701d24f421141eee
Samir_Badi <samirbadi1@outlook.com>
2022-03-13 19:35:44 -0400

    Add the class AdminService in service

--- src/main/java/Javatown/Main.java
+++ src/main/java/Javatown/Main.java
@@ -2,6 +2,7 @@ package Javatown;
 
 import Javatown.persistence.ClientDaoJPAH2;
 import Javatown.persistence.EmployeDaoJPAH2;
+import Javatown.service.AdminService;
 import Javatown.service.ClientService;
 import Javatown.service.EmployeService;
 
@@ -9,5 +10,6 @@ public class Main {
     public static void main(String[] args) {
         ClientService clientService = new ClientService(new ClientDaoJPAH2());
         EmployeService employeService = new EmployeService(new EmployeDaoJPAH2());
+        AdminService adminService = new AdminService(new AdminDaoJPAH2);
     }
 }
---
+++ src/main/java/Javatown/service/AdminService.java
@@ -0,0 +1,8 @@
+package Javatown.service;
+
+public class AdminService {
+    private AdminDao dao;
+    public AdminService(AdminDao dao) {
+        this.dao = dao;
+    }
+}
