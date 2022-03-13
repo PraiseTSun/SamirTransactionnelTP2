e047272942c4ee848b35682dc063c6dcecdb178e
Samir_Badi <samirbadi1@outlook.com>
2022-03-13 18:07:01 -0400

    Add the abstract class AbstractUser to the couche modele

---
+++ src/main/java/Javatown/modele/AbstractUser.java
@@ -0,0 +1,43 @@
+package Javatown.modele;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class AbstractUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @ToString.Exclude
+    private String password;
+
+    public AbstractUser(String firstName, String lastName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
