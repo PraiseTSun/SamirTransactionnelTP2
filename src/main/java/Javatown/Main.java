a2323e3f0b3e24c1d3137a2a622fb0e8dc0bb69e
Samir_Badi <samirbadi1@outlook.com>
2022-03-13 19:33:29 -0400

    Add the class EmployeDaoJPAH2 in persistence

--- src/main/java/Javatown/Main.java
+++ src/main/java/Javatown/Main.java
@@ -1,12 +1,13 @@
 package Javatown;
 
 import Javatown.persistence.ClientDaoJPAH2;
+import Javatown.persistence.EmployeDaoJPAH2;
 import Javatown.service.ClientService;
 import Javatown.service.EmployeService;
 
 public class Main {
     public static void main(String[] args) {
         ClientService clientService = new ClientService(new ClientDaoJPAH2());
-        EmployeService employeService = new EmployeService(new EmployeDaoJPAH2);
+        EmployeService employeService = new EmployeService(new EmployeDaoJPAH2());
     }
 }
---
+++ src/main/java/Javatown/persistence/EmployeDaoJPAH2.java
@@ -0,0 +1,4 @@
+package Javatown.persistence;
+
+public class EmployeDaoJPAH2 extends BaseDaoH2 implements EmployeDao {
+}
